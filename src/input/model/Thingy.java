a5df2033f83155f4c7b83f10bf66e5035261c4d9
JordanBurnett1210 <jabur1210@gmail.com>
2015-09-30 08:25:51 -0600

    added setters to the thingy class
    added setters and documentation to the thingy class.

--- src/input/model/Thingy.java
+++ src/input/model/Thingy.java
@@ -7,23 +7,33 @@ package input.model;
  */
 public class Thingy
 {
+	
 	private String name;
 	private int age;
 	private double weight;
 	
+	/**
+	 * Sets the default values for name, age, and weight. They are ridiculous values.
+	 */
 	public Thingy()
 	{
 		name = "";
 		age = -99;
 		weight = -.008;
 	}
-	
+	/**
+	 * 
+	 * @param name
+	 * @param age
+	 * @param weight
+	 */
 	public Thingy(String name, int age, double weight)
 	{
 		this.name = name;
 		this.age = age;
 		this.weight= weight;
 	}
+	
 	
 	public String getName()
 	{
@@ -33,10 +43,21 @@ public class Thingy
 	{
 		return age;
 	}
-	
 	public double getWeight()
 	{
 		return weight;
 	}
 
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	public void setWeight(double weight)
+	{
+		this.weight = weight;
+	}
 }
